030ee5e2ce786b8e19184840ada5e14986ed8c63
Niki Ahlskog <niki.ahlskog@gmail.com>
2018-07-09 12:44:27 +0300

    muokkaus

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import json
 tietokanta = "tietokanta.json"
 
 def begin():
-    print('Baarikierros softa versio 1.0 \n 0. Apu \n 1. Lisää baarikierros \n 2. Listaa baarikierrokset \n 3. Poista baarikierros \n 4. Lopeta \n')
+    print('Baarikierros softa versio 1.0 \n 0. Apu \n 1. Lisää baarikierros \n 2. Listaa baarikierrokset \n 3. Poista baarikierros \n 4. Muokkaa kierrosta \n 5. Lopeta \n')
     start()
 
 def apua():
@@ -12,8 +12,8 @@ def apua():
     print('1. Lisää baarikierros')
     print('2. Listaa baarikierrokset')
     print('3. Poista baarikierros')
-    print('4. Lopeta\n')
-    start()
+    print('4. Muokkaa kierrosta')
+    print('5. Lopeta\n')
 
 def exit():
     print('Kiitos ja näkemiin! \n')
@@ -36,22 +36,24 @@ def addBaarikierros():
     with open(tietokanta, 'w') as f:
         json.dump(data, f)
     print('Kierros lisätty onnistuneesti\n')
-    start()
 
-def listOnly():
+def listBaarikierrokset():
+    with open(tietokanta) as json_file:
+        data = json.load(json_file)
+        for index, kierros in enumerate(data['Baarikierrokset']):
+            print(index, kierros['name'])
+            print('  ', 'Lokaatio:', kierros['location'])
+            print('  ', 'Aika:', kierros['time'])
+            print('  ', 'Juomien määrä:', kierros['drinkAmount'])
+    print('\n')
+
+def removeBaarikierros():
+    print('Poista kierros:\n')
     with open(tietokanta) as json_file:
         data = json.load(json_file)
         for index, kierros in enumerate(data['Baarikierrokset']):
             print(index, kierros['name'])
     print('\n')
-
-def listBaarikierrokset():
-    listOnly()
-    start()
-
-def removeBaarikierros():
-    print('Kierrokset:\n')
-    listOnly()
     index = int(input('Mikä kierros poistetaan? Anna numero.\n'))
     with open(tietokanta) as json_file:
         data = json.load(json_file)
@@ -59,25 +61,52 @@ def removeBaarikierros():
     with open(tietokanta, 'w') as f:
         json.dump(data, f)
     print('Poistettu onnistuneesti.\n')
-    start()
+
+def editKierros():
+    print('Muokkaa kierrosta:\n')
+    with open(tietokanta) as json_file:
+        data = json.load(json_file)
+        for index, kierros in enumerate(data['Baarikierrokset']):
+            print(index, kierros['name'])
+    print('\n')
+    index = int(input('Mitä kierrosta muokataan? Anna numero.\n'))
+    print(data['Baarikierrokset'][index]['name'])
+    name = input('uusi nimi kierrokselle:') or data['Baarikierrokset'][index]['name']
+    print(data['Baarikierrokset'][index]['location'])
+    location = input('uusi lokaatio kierrokselle:') or data['Baarikierrokset'][index]['location']
+    print(data['Baarikierrokset'][index]['time'])
+    time = input('uusi aika kierrokselle:') or data['Baarikierrokset'][index]['time']
+    print(data['Baarikierrokset'][index]['drinkAmount'])
+    drinkAmount = input('Juomien määrä kierrokselle:') or data['Baarikierrokset'][index]['drinkAmount']
+    data['Baarikierrokset'][index]['name'] = name
+    data['Baarikierrokset'][index]['location'] = location
+    data['Baarikierrokset'][index]['time'] = time
+    data['Baarikierrokset'][index]['drinkAmount'] = drinkAmount
+    with open(tietokanta, 'w') as f:
+        json.dump(data, f)
+    print('Muokattu onnistuneesti.\n')
+
 
 def start():
-    toDo = input('Kuis laitetaa? Syötä numero:\n')
-    if toDo == '0':
-        print('\n')
-        apua()
-    elif toDo == '1':
-        print('\n')
-        addBaarikierros()
-    elif toDo == '2':
-        print('\n')
-        listBaarikierrokset()
-    elif toDo == '3':
-        print('\n')
-        removeBaarikierros()
-    elif toDo == '4':
-        exit()
-    else:
-        print('Vain numeroita 1-3 pls')
+    while True:
+        toDo = input('Kuis laitetaa? Syötä numero:\n')
+        if toDo == '0':
+            print('\n')
+            apua()
+        elif toDo == '1':
+            print('\n')
+            addBaarikierros()
+        elif toDo == '2':
+            print('\n')
+            listBaarikierrokset()
+        elif toDo == '3':
+            print('\n')
+            removeBaarikierros()
+        elif toDo == '4':
+            editKierros()
+        elif toDo == '5':
+            exit()
+        else:
+            print('Vain numeroita 1-3 pls')
 
 begin()
